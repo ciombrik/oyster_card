ffacd4f5446f87bca17dbd074a6b0b0a2edb5ef2
Mike Connolly <mjc86@hotmail.co.uk>
2021-06-07 16:08:16 +0100

    end of ch6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  CARD_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def topup(amount)
+    raise "Can't top-up, card limit is £90" if @balance >= Oystercard::CARD_LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,9 +8,19 @@ describe Oystercard do
     expect(oystercard.balance).to eq 0
   end
 
-  it 'adds money to the card' do
-    oystercard.topup(20)
-    expect(oystercard.balance).to eq 20
+  describe '#topup' do
+
+    it 'adds money to the card' do
+      oystercard.topup(20)
+      expect(oystercard.balance).to eq 20
+    end
+
+    context 'when card is at limit' do
+      it 'raises an error' do
+        oystercard.topup(90)
+        expect { oystercard.topup(1) }.to raise_error "Can't top-up, card limit is £90"
+      end
+    end
   end
 
 end
